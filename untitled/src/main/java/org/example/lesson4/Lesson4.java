42c29a139284ff505d33156f9cef690467545004
tatarenkova <*S@xS34G35>
2023-03-27 17:46:50 +0300

    4

--- untitled/src/main/java/org/example/lesson4/Lesson4.java
+++ untitled/src/main/java/org/example/lesson4/Lesson4.java
@@ -1,43 +1,61 @@
 package main.java.org.example.lesson4;
 
+import java.util.Arrays;
+
 public class Lesson4 {
     public static void main(String[] args) {
-        int[] result = new int[]{2,4,6,7};
-
+        int[] result = sortMerge(new int[]{8,2,7,6});
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
     }
 
-    public static int[] merge(int i, int j) {
-        int[] result = new int[2];
-        if ( i> j) {
-            result[0] = i;
-            result[1] = j;
-        }
-        if ( i <= j) {
-            result[0] = j;
-            result[1] = i;
+    public static int[] merge(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
+        int index = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++){
+                if (arr1[i] <= arr2[j]) {
+                    result[index] = arr1[i];
+                    index++;
+                    if ( j == arr2.length-1){
+                        result[index] = arr2[j];
+                        index++;
+                    }
+                    break;
+                } else {
+                    result[index] = arr2[j];
+                    index++;
+                    if ( j == arr2.length-1){
+                        result[index] = arr1[i];
+                        index++;
+                    }
+                }
+            }
+
         }
         return result;
     }
 
-   /* public static int[] sortMerge(int[] arr) {
-        if (arr.length > 2) {
-            return merge(arr[0], arr[1]);
+    public static int[] sortMerge(int[] arr) {
+        if (arr.length <= 1) {
+            return arr;
         }
-        int indexMid = arr.length/2;
+        int indexMid = arr.length / 2;
         int[] arr1 = new int[indexMid];
         int[] arr2 = new int[indexMid];
         int newIndex = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (i < indexMid){
+            if (i < indexMid) {
                 arr1[i] = arr[i];
             } else {
                 arr2[newIndex] = arr[i];
                 newIndex++;
             }
         }
-        sortMerge(arr1);
-        sortMerge(arr2);
-    }*/
+        int[] result = merge(sortMerge(arr1), sortMerge(arr2));
+        return result;
+    }
 
     public static boolean anagram(int[] arr1, int[] arr2){
         return true;
